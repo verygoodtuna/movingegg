d7dc974c8fb1071d4c850c7b555c5a6d7bbd5b1b
verygoodtuna <kjy990504@gmail.com>
2023-06-29 18:17:42 +0900

    movingegg

--- js/egg.js
+++ js/egg.js
@@ -1,3 +1,18 @@
+const docuE = document.documentElement;
+const newCursor = document.getElementById("circularCursor");
+let posX = 0;
+let posY = 0;
+
+document.addEventListener('DOMcontentLoaded', () => {
+    document.body.onmusemove = function(e) {
+    posX = e.clientX + "px";
+    posY = e.clientY + "px";
+
+    newCursor.style.left = posX;
+    newCursor.style.top = posY;
+}
+});
+
 $(function () {
     $('.e_slide').slick({
         arrows: false,
@@ -5,5 +20,27 @@ $(function () {
         slidesToShow: 1,
         centerMode: true,
         centerPadding: '200px',
+    })
+
+    $('.slide_btn .egg_l').on('click', function () {
+        $('.e_slide').slick('slickPrev')
+    })
+
+    $('.slide_btn .egg_r').on('click', function () {
+        $('.e_slide').slick('slickNext')
+    })
+
+    $('.r_slide').slick({
+        arrows: false,
+        fade: true,
+    })
+
+    
+    $('.r_btn .b_left').on('click', function () {
+        $('.r_slide').slick('slickPrev')
+    })
+
+    $('.r_btn .b_right').on('click', function () {
+        $('.r_slide').slick('slickNext')
     })
 })
